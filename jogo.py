023b6a04fd35551eb30f24beb0c2fed61a701aa1
roberta-barros <robertabarrost@gmail.com>
2022-11-09 13:41:09 -0300

    oi

--- jogo.py
+++ jogo.py
@@ -1,1 +1,6 @@
-print('oi')
+print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!')
+
+nome = input('Qual seu nome? ')
+
+print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')                     
+print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
