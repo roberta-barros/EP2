b7435a2be4021ce7219bd51c745533109b5035de
joaoheinke <joaovtito@gmail.com>
2022-11-09 14:35:46 -0300

    teste2

--- jogo.py
+++ jogo.py
@@ -4,3 +4,4 @@ nome = input('Qual seu nome? ')
 
 print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')                     
 print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
+print('deu certo')
