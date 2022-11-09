6af84dfa7321b27912a22c3b7e4ba060afcba7fb
roberta-barros <robertabarrost@gmail.com>
2022-11-09 14:54:22 -0300

    jogar_novamente

--- jogo.py
+++ jogo.py
@@ -1,10 +1,14 @@
 import funcoes
 import perguntas
 
-jogo_correto = True
+jogar_novamente = True
 
-while jogo_correto == True:
-    
+while jogar_novamente == True:
+    questoes = 1
+    questoes_corretas = 0
+    respostas_lista = ['A', 'B', 'C', 'D']
+    todas_respostas_lista = ['A', 'B', 'C', 'D', 'pular', 'ajuda', 'parar']
+    pergunta = perguntas.quest
     base_de_questoes = funcoes.transforma_base(perguntas)
     print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!')
     nome = input('Qual seu nome? ')
