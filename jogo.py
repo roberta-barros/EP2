43340a8a336fb2de522811108388d7c42e33694b
joaoheinke <joaovtito@gmail.com>
2022-11-09 14:53:13 -0300

    começo jogo

--- jogo.py
+++ jogo.py
@@ -10,3 +10,7 @@ while jogo_correto == True:
     nome = input('Qual seu nome? ')
     print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')                     
     print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
+    print('Aperte ENTER para continuar...')
+    print('O jogo já vai começar! Lá vem a primeira questão!')
+    print('Vamos começar com as questões do nível FACIL!')
+    print('Aperte ENTER para continuar...')
