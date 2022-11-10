e7e3a080f07952217f04139a7490916556b5d5b8
roberta-barros <robertabarrost@gmail.com>
2022-11-09 23:24:09 -0300

    valida

--- jogo.py
+++ jogo.py
@@ -8,8 +8,10 @@ while jogar_novamente == True:
     questoes_corretas = 0
     respostas_lista = ['A', 'B', 'C', 'D']
     todas_respostas_lista = ['A', 'B', 'C', 'D', 'pular', 'ajuda', 'parar']
+    premios = [1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
     pergunta = perguntas.quest
-    base_de_questoes = funcoes.transforma_base(perguntas)
+    base_questoes = funcoes.transforma_base(perguntas)
+    valida_base = funcoes.valida_questoes(perguntas)
     print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!')
     nome = input('Qual seu nome? ')
     print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')                     
@@ -18,3 +20,9 @@ while jogar_novamente == True:
     print('O jogo já vai começar! Lá vem a primeira questão!')
     print('Vamos começar com as questões do nível FACIL!')
     print('Aperte ENTER para continuar...')
+    if valida_base != {}:
+        print('ERRO! O jogo não pode ser iniciado')
+    else:
+        for i in range(len(pergunta)):
+            questao_testada = funcoes.questao_para_texto(pergunta)
+            print(questao_testada)
