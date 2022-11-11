56490f9cb3be2b72804139d48adacd25987ee6d8
roberta-barros <robertabarrost@gmail.com>
2022-11-10 23:35:52 -0300

    ajuda

--- jogo.py
+++ jogo.py
@@ -13,6 +13,7 @@ while jogar_novamente == True:
     base_questoes = funcoes.transforma_base(lista_perguntas)
     questoes_sorteadas = []
     x = True
+    jogar = True
     niveis = ['facil', 'medio', 'dificil']
     for nivel in niveis:
         validar_base = base_questoes[nivel]
@@ -21,12 +22,25 @@ while jogar_novamente == True:
             if base_validada[i] != {}:
                 print('ERRO! O jogo não pode ser iniciado.')
                 x = False
+                jogar = False
     if x == True:
         print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!')
         nome = input('Qual seu nome? ')
         print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')                     
         print('As opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
-        print('Aperte ENTER para continuar...')
         print('O jogo já vai começar! Lá vem a primeira questão!')
         print('Vamos começar com as questões do nível FACIL!')
-        print('Aperte ENTER para continuar...')
+    while jogar == True:
+        if questoes <= 3:
+            nivel = 'facil'
+            questao_sorteada = funcoes.sorteia_questao_inedita(base_questoes, nivel, questoes_sorteadas)
+            questao_printada = funcoes.questao_para_texto(questao_sorteada, id)
+            print(questao_printada)
+            resposta = input('Qual sua resposta? ')
+            if resposta not in todas_respostas_lista:
+                print('Opção inválida! \nAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
+            else:
+                if resposta == 'ajuda':
+                    ajuda = funcoes.gera_ajuda(questao_sorteada)
+                    print('Ok, lá vem ajuda! Você ainda tem 1 ajudas!')
+                    print(f'{ajuda}')
